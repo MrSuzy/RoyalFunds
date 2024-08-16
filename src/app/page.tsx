c0316b45c14e726667b216c23c46fc639c25531b
irisss0209 <145740565+irisss0209@users.noreply.github.com>
2024-08-16 13:45:32 +0800

    header-changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,18 +17,22 @@ export default function HomePage() {
     <Header/>
     
 
-      <div className="Container">
-        <AspectRatio ratio={16 / 4} className="bg-muted">
+        <div className="Container" >
+        <AspectRatio ratio={16 /4} className="bg-muted ">
           <Image
-            src="/Chain-Funds (2).svg"
+            src="/header5.webp"
             alt="Photo by Drew Beamer"
             fill
-            className="rounded-2xl object-cover"
+            style={{ width: '100%', filter: 'blur(2px)' }} 
+            className="rounded-2x1 object-contain"
           />
         </AspectRatio>
-
-        
+        <div style={{ position: 'absolute', top: '30%', left: '10%', transform: 'translate(-20%, 0%)', color: 'white', textAlign: 'left' }}>
+        <h1 style={{ fontSize: '68px', fontWeight: '700', fontFamily: 'Roboto, sans-serif', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.6)' }}>Royal Funds</h1>
+        <h2 style={{ fontSize: '28px',fontWeight: '400',textShadow: '2px 2px 4px rgba(0, 0, 0, 0.6)' }}>Funding everywhere</h2>
+  </div>
       </div>
+
       <Header2/>
       <div className="flex justify-center items-center mx-auto max-w-screen-lg p-4"> 
         <BentoGridDemo/>
